5b43e79cc2973c9d262b84c691beb7e2381aba91
SikmaLegenseed <agungsurya0042@gmail.com>
2025-01-21 20:54:11 +0800

    added animation in js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-// Fungsi untuk melakukan scroll halus ke bagian konten
-function scrollToContent() {
-    const contentSection = document.querySelector('.content');
-    contentSection.scrollIntoView({ behavior: 'smooth' });
-  }
+// Get the button element
+const button = document.querySelector('.button-main');
 
-  // Dapatkan elemen tombol dan tambahkan event listener
-  const readMoreButton = document.querySelector('.button-main');
-  readMoreButton.addEventListener('click', scrollToContent);
+// Add a click event listener to the button
+button.addEventListener('click', function() {
+    // Get the content section element
+    const content = document.querySelector('.content');
+
+    // Scroll to the content section
+    content.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const contentImage = document.querySelector('.content img');
+
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              contentImage.style.opacity = "1";
+               contentImage.style.transform = "translateY(0)";
+          }
+      });
+  });
+
+    observer.observe(contentImage);
+});
