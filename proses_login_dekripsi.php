d6e53b0eeff4e538ad6214b22e5d745ebf6a089d
SikmaLegenseed <agungsurya0042@gmail.com>
2025-02-03 23:21:36 +0800

    final login decryption

--- proses_login_dekripsi.php
+++ proses_login_dekripsi.php
@@ -4,7 +4,7 @@ session_start();
 $user = $_POST['username'];
 $pass = $_POST['password'];
 
-$query = mysqli_query($conn, "SELECT * FROM tb_user WHERE username = '$user'");
+$query = mysqli_query($conn, "SELECT * FROM tb_user WHERE username='$user'");
 $baris_level = mysqli_fetch_assoc($query);
 $cek = password_verify($pass, $baris_level['password']);
 
@@ -16,5 +16,4 @@ if ($cek){
 }else{
     echo "<script>alert('Gagal Login!');window.location.href='login.php'</script>";
 }
-
 ?>
